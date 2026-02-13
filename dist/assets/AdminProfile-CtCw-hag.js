542d71a9fe8f9080bee12c15d4e71afb678139fd
Thiago Fonseca <thiago@odeducacao.com.br>
2026-02-13 18:12:13 -0300

    Ignore dist build output

--- dist/assets/AdminProfile-CtCw-hag.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-HcuLVkXM.js";import o from"./Profile-H4CorJvq.js";const i=()=>r.jsx(o,{});export{i as default};
--- dist/assets/ContentsCourses-BOi_1Huo.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-HcuLVkXM.js";import{C as t}from"./ContentsList-DARi6ScV.js";import"./plus-DDz1DOCh.js";import"./x-Dm__mln4.js";const i=()=>s.jsx(t,{type:"course",title:"Cursos",subtitle:"Explore cursos gravados disponíveis para sua clínica.",basePath:"/contents/courses"});export{i as default};
